f0c9a3740d0479a8194b80933dcc56190bc6f7ed
Julian Behrends <julian.behrends@campus.tu-berlin.de>
2023-05-05 15:29:40 +0000

    implement fen move
    - Moves can now be input in fen notation.
    
    Note: this needs #4 to be implemented to work properly

--- projekt_ki_c/src/main.c
+++ projekt_ki_c/src/main.c
@@ -12,29 +12,49 @@ int main(int argc, char **argv) {
     // np->black =         0b1111111111111111000000000000000000000000000000000000000000000000;
     // np->white =         0b0000000000000000000000000000000000000000000000001111111111111111;
     board_state *np = fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"); //"r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1"
+    np->player = 1;
 
     //simple move interface for testing
     while (1) {
         print_board(np);
+        printf("player: %d\n", np->player);
 
         //read move accepts move in format: XY*XY examples: A2-A3, B2 B3
         char *move = NULL;
         size_t len = 0;
 
         if (getline(&move, &len, stdin) != -1) {
-            if (strlen(move) >= 5) {
+            if (strlen(move) >= 6) {
+                board_move* b_move = malloc(sizeof(board_move));
+                memset(b_move, 0, sizeof(board_move));
+                b_move->piece = -1; //unknown piece type
                 //convert move coordinates
                 //note: does not check for invalid input
-                __uint64_t from =   (__uint64_t)1 << (7 - (move[0] - 'A') + (move[1] - '1') * 8);
-                __uint64_t to =     (__uint64_t)1 << (7 - (move[3] - 'A') + (move[4] - '1') * 8);
-                if (perform_move(np, from, to) != 0) {
-                    printf("invalid move!\n");
+                b_move->from =   (__uint64_t)1 << (7 - (toupper(move[0]) - 'A') + (move[1] - '1') * 8);
+                b_move->to =     (__uint64_t)1 << (7 - (toupper(move[3]) - 'A') + (move[4] - '1') * 8);
+
+                int move_code = 0;
+                if ((move_code = perform_move(np, b_move)) != 0) {
+                    printf("invalid move!%d\n", move_code);
                 }
+                free(b_move);
+            } else if (strlen(move) >= 3) {
+                board_move* b_move = fen_to_move(move, np);
+
+                int move_code = 0;
+                if (b_move == NULL || (move_code = perform_move(np, b_move)) != 0) {
+                    printf("invalid move!%d\n", move_code);
+                }
+                free(b_move);
             } else if (strlen(move) >= 1) {
-                if (move[0] == 'q')
+                if (move[0] == 'q') {
+                    free(move);
                     break;
+                }
             }
         }
+
+        free(move);
     }
 
 
